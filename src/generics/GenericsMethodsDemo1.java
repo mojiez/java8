78e3ab70539c0f9281da74e17a9b72f53f8ecbf8
mojiez <937691466@qq.com>
2024-11-06 16:45:59 +0800

    泛型学习02

--- src/generics/GenericsMethodsDemo1.java
+++ src/generics/GenericsMethodsDemo1.java
@@ -78,11 +78,92 @@ class Container<T> {
         return type.newInstance();
     }
 }
+
+/*
+泛型的上下限
+ */
+class A{}
+class B extends A{}
+
 public class GenericsMethodsDemo1 {
+//    /**
+//     * 泛型的上下限
+//     */
+//    public static void funA(A a) {
+//
+//    }
+//
+//    /**
+//     * 这样调用没问题， 本质上就是子类可以自动向上转化为父类
+//     * @param b
+//     */
+//    public static void funB(B b) {
+//        funA(b);
+//    }
+//
+//    public static void funC(List<A> listA) {
+//
+//    }
+//
+//    /**
+//     * 这样调用是有问题的
+//     * // 编译器检查 funC(listB) 调用时：
+//     * // funC 期望参数类型：List<A>
+//     * // 实际传入参数类型：List<B>
+//     *
+//     * // 虽然 B extends A
+//     * // 但是 List<B> 不是 List<A> 的子类型！
+//     * @param listB
+//     */
+//    public static void funD(List<B>  listB) {
+//        funC(listB);
+//    }
+
+    /*
+    解决方法
+    <? extends A> 表示该类型参数可以是A或者A的子类类型 这是泛型的上限
+    编译时擦除到A
+    类型检查阶段
+    // funE 的参数声明：List<? extends A>
+    // 这表示：接受任何 List，其元素类型是 A 或 A 的子类
+
+    // 检查 funE(listB) 调用：
+    // listB 的类型是 List<B>
+    // B 是 A 的子类
+    // 所以 List<B> 符合 List<? extends A> 的要求
+
+    编译阶段
+    // 编译前（带有泛型信息）
+    List<? extends A> listA
+
+    // 擦除后（保留边界类型A）
+    List listA  // 内部实际引用的类型是 A，而不是 Object
+
+    // 编译器生成等价字节码代码
+    // 源代码
+    public static void funE(List<? extends A> listA) {
+        A a = listA.get(0);
+    }
+
+    // 擦除后的代码
+    public static void funE(List listA) {
+        A a = (A) listA.get(0);  // 编译器插入从Object到A的转换
+    }
+     */
+    public static void funE(List<? extends A> listA) {
+
+    }
+
+    public static void funF(List<B> listB) {
+        funE(listB);
+    }
+
+
     public static void main(String[] args) {
-        List<Object> list = new ArrayList<>();
-        list.add(new Cat());
-        list.add(new Dog());// 这样的操作是可以的， 因为List里面存放的是Object类型， Dog 和 Cat实际上进行了类型转化（自动向上类型转化）
-        //
+//        List<Object> list = new ArrayList<>();
+//        list.add(new Cat());
+//        list.add(new Dog());// 这样的操作是可以的， 因为List里面存放的是Object类型， Dog 和 Cat实际上进行了类型转化（自动向上类型转化）
+
+
     }
 }
---
+++ src/generics/GenericsUpLowDemo.java
@@ -0,0 +1,106 @@
+package generics;
+
+import java.util.List;
+
+/**
+ * 泛型上下限的引入
+ * 注意
+ * 类泛型 只能定义T 和 T extends Number
+ * 不能定义 T super
+ * 即 类泛型不能定义 下限
+ * // 如果允许 T super String
+ * class Info2<T super String> {
+ *     private T var;  // 擦除后应该变成什么类型？
+ *     // Object？那么就失去了下界的意义
+ *     // String？那么可能不兼容T的实际类型
+ * }
+ */
+class Info1 <T extends Number> { //此泛型的上限是Number
+    private T var;
+
+    @Override
+    public String toString() {
+        return "Info1{" +
+                "var=" + var +
+                '}';
+    }
+
+    public T getVar() {
+        return var;
+    }
+
+    public void setVar(T var) {
+        this.var = var;
+    }
+}
+
+class Info2<T> {
+    private T var;
+
+    @Override
+    public String toString() {
+        return "Info2{" +
+                "var=" + var +
+                '}';
+    }
+
+    public T getVar() {
+        return var;
+    }
+
+    public void setVar(T var) {
+        this.var = var;
+    }
+}
+public class GenericsUpLowDemo {
+    public static void main(String[] args) {
+        // 可以接收Number 和 Number以下的
+        Info1<Integer> info = new Info1<>();
+        // Info1<String> info2 = new Info1<String>(); 这样写就是错的
+
+
+    }
+    /**
+     * 下限
+     * super的正确用法用在泛型方法中
+     */
+    public static void fun(Info2<? super String> temp) { // 只接收String和String以上的（Object）
+
+    }
+    public static void funA(List<? super String> list) {
+        list.add("nihao");
+        Object s = list.get(0); // 这样写是可以的，因为 ? 不确定是什么
+//        String s1 = list.get(0); // 这样写不可以， 因为类型检查阶段， 不确定给list传的是Object 还是 String， 所以不能这样取
+    }
+
+    /*
+    为什么不能写 T super String
+    下面这段代码报错
+    原因是T应该是一个确定的类型（在整个类和方法中表示同一个具体的类型）
+    如果写 T super String, T就不是一个确定的了， 它可以是String  可以是Object  定义冲突
+    这违背了设计T的初衷
+     */
+//    public static <T> void funB(Info2<T super String>) {
+//
+//    }
+
+    /**
+     * 泛型方法中使用T的例子
+     * 这两个方法中的
+     */
+    public static <T extends Number> double sum(List<T> numbers) {
+        double sum = 0;
+        for (T number : numbers) {
+            sum += number.doubleValue();
+        }
+        return sum;
+    }
+    // 这样写有点不优雅， 上面的写法比较好
+    public static <T> double sum1(List<T> numbers) {
+        double sum = 0;
+        for (T number : numbers) {
+            sum += (double) number;
+        }
+        return sum;
+    }
+}
